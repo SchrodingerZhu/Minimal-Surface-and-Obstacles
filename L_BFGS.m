4e1bb2a78c46e0548f8cdf723a2a442f80f45d02
Schrodinger ZHU <i@zhuyi.fan>
2020-12-30 10:42:34 +0800

    adjust interface and add qpm

--- L_BFGS.m
+++ L_BFGS.m
@@ -3,20 +3,32 @@
 %% Created by Schrodinger ZHU <i@zhuyi.fan> for DDA/CIE6010 Project
 %%
 %% USAGE:
+%%  1.       f, objective function
+%%  2.       g, gradient function
+%%  3.      x0, initial point
+%%  4.    opts, options
+
+%% OPTIONS
 %%  1.       s, initial value of line searching
 %%  2.   sigma, line searching exponential scaling factor
 %%  3.   gamma, line searching condition scaling factor
-%%  4.      x0, initial point
-%%  5.       m, memory factor
-%%  6. epsilon, terminating tolerance
-%%  7.   delta, skipping tolerance
-%%  8.       f, objective function
-%%  9.       g, gradient function
-%% 10.       H, initial matrix of memoization
+%%  4.       m, memory factor
+%%  5. epsilon, terminating tolerance
+%%  6.   delta, skipping tolerance
+%%  7.       H, initial matrix of memoization
 %%
 
 
-function [ opt, x, G ] = L_BFGS (s, sigma, gamma, x0, m, epsilon, delta, f, g, H)
+function [ x, opt, G ] = L_BFGS (f, g, x0, opts)
+    s       = opts.s;
+    sigma   = opts.sigma;
+    gamma   = opts.gamma; 
+    m       = opts.m;
+    epsilon = opts.epsilon;
+    delta   = opts.delta;
+    H       = opts.H;
+
+    
     S    = {}; % store all s^k = difference of x
     Y    = {}; % store all y^k = difference of gradient
     RHO  = {}; % store all 1/<s^k, y^k> to reduce the computation
---
+++ qpm.m
@@ -0,0 +1,61 @@
+%%
+%% Quadratic Penalty Method
+%% Created by Schrodinger ZHU <i@zhuyi.fan> for DDA/CIE6010 Project
+%%
+%% USAGE:
+%%  1.       f, objective function
+%%  2.       g, gradient of objective function 
+%%  3.       G, inequality constraint functions  (cell array)
+%%  4.      GG, inequality gradients (cell array)    
+%%  5.       H, equality constraint functions (cell array)
+%%  6.      GH, equality gradients (cell array)
+%%  7.      x0, initial point
+%%  8.  solver, unconstraint solver
+%%  9.  s_opts, solver options
+%% 10. penalty, penalty parameter (> 1)
+%% 11,     eps, constraint checking tolerance
+%%
+
+function [x, opt] = qpm(f, g, G, GG, H, GH, x0, solver, s_opts, penalty, eps)
+    p = @(x)(P(x, f, g, G, H, penalty));
+    grad = @(x)(Grad(x, g, G, GG, H, GH, penalty));
+    x = solver(p, grad, x0, s_opts);
+    while not(check(x, G, H, eps))
+        penalty = penalty * penalty;
+        p = @(x)(P(x, f, g, G, H, penalty));
+        grad = @(x)(Grad(x, g, G, GG, H, GH, penalty));
+        x = solver(p, grad, x0, s_opts);
+    end
+    opt = f(x);
+end
+
+function [res] = check(x, G, H, eps)
+    res = true;
+    for i = 1:length(G)
+        res = res && (G{i}(x) <= eps);
+    end
+    for i = 1:length(H)
+        res = res && (H{i}(x) <= eps) && (H{i}(x) >= -eps);
+    end
+end
+
+function [res] = P(x, f, G, H, penalty)
+    res = f(x);
+    for i = 1:length(G)
+        res = res + 0.5 * penalty * max(0, G{i}(x))^2;
+    end
+    for i = 1:length(H)
+        res = res + 0.5 * penalty * H{i}(x)^2;
+    end
+end
+
+function [res] = Grad(x, g, G, GG, H, GH, penalty)
+    res = g(x);
+    for i = 1:length(GG)
+        res = res + penalty * max(0, G{i}(x)) * GG{i}(x);
+    end
+    for i = 1:length(H)
+        res = res + penalty * H{i}(x) * GH{i}(x);
+    end
+end
+
