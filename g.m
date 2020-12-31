2ea042fe7dcdaab2dfc2395a7d2fd7c301fda72b
Matlavo <53594256+Matlavo@users.noreply.github.com>
2020-12-31 23:10:58 +0800

    Delete g.m

--- g.m
+++
@@ -1,8 +0,0 @@
-function y = g(x, m, n, r)
-    if length(x) ~= (m-2)*(n-2)
-        fprintf("Wrong dimension!\n")
-    end
-    X = reshape(x, m-2, []);
-    y = manip2(addbd(X, r))';
-    y = y / 2;
-end
