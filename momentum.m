239e6c71a7e6b9c84f7b2cf035acca956fba983c
Matlavo <53594256+Matlavo@users.noreply.github.com>
2020-12-31 16:22:05 +0800

    Momentum

--- momentum.m
+++ momentum.m
@@ -1,25 +1,39 @@
-function [x,objv,iter] = momentum(obj, grad, x0,opts)
-fprintf("momentum");
-fprintf("ITER ; OBJ.VAL; STEP.SIZE\n");
+% opts.maxit = 20000; opts.tol = 10^(-5);
+% opts.gamma = 0.1; opts.s = 0.001;
+% opts.sigma = 0.5; opts.m = 1; opts.e = 10^(-7);
+function [x,obj,iter] = momentum(f, grad, x0, opts)
 iter = 0;
-x_old = zeros(size(x0));
 x = x0;
-% Other rules to select step size
-while norm(grad(x)) > opts.tol
-    beta = 2/(iter+2);
-    %y = x + beta*(x - x_old);
-    d = -grad(x);
+x_old = zeros(size(x0));
+g = grad(x);
+ng = norm(g);
+
+fprintf("ITER ; OBJ.VAL ; GRAD.NORM ; STEP.SIZE\n")
+while ng > opts.tol
+    beta = 2/(iter+2); % Other rules to select step size
+    y = x + beta*(x-x_old);
+    g = grad(x);
+    d = -g;
+    ng = norm(g);
     alpha = opts.s;
     i = 0;
-    while (obj(x + alpha * d) - obj(x) >= -opts.gamma * alpha * norm(d)^2) && (i < 5)
+    
+    while (f(x + alpha * d)) - f(x) >= -opts.gamma*alpha*ng^2% &&(i <10^4)
         alpha = alpha * opts.sigma;
-        i = i+1;
+        i = i + 1;
     end
-    xtemp = x + alpha * d + beta*(x - x_old);
+    
+    fprintf("NUM.BACKTRACK = %u\n", i)
+    
+    fprintf("[%4i] ; %2.6f ; %2.6f ; %2.6f\n", iter, f(x), ng, alpha);
+
+    xtemp = y + alpha*d;
+    
     x_old = x;
     x = xtemp;
+    
     iter = iter + 1;
-    fprintf("[%4i] ; %2.6f ; %1.4f\n", iter, obj(x),  alpha)
 end
-    objv = obj(x);
+    
+    obj = f.obj(x);
 end
